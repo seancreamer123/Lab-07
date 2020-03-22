14b0071821d3ee630bb2d1951ae0e66904c1b235
Sean <seancreamer30@gmail.com>
2020-03-22 01:49:50 -0400

    first commit

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+userName = input("Hello! Please enter your facebook username: ")
+
+userPassword = input("Hello! Please enter your facebook password: ")
+
+if(userName.lower() == "sean" and userPassword == "Pa$$w0rd!"):
+    print("Welcome to facebook!")
+else:
+    print("Wrong credentials, try again!")
+
+
